d4f4f68fed7eb5a083125714689926fa94f8b8a3
Alex <alexk@yess.ai>
2024-09-12 23:59:13 +0200

    Linkedin List, shit and pop implemented

--- src/linkedList/LinkedList.ts
+++ src/linkedList/LinkedList.ts
@@ -31,9 +31,44 @@ export class SinglyLinkedList {
         this.tail = newNode;
       }
     }
-    
+
     this.length++;
 
     return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+
+    this.length = this.length - 1;
+
+    return current;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+
+    const currentHead = this.head;
+
+    this.head = this.head.next;
+    this.length--;
+
+    // if list is empty we also to update tail
+    if (this.length === 0) {
+      this.tail = null;
+    }
+
+    return currentHead;
   }
 }
